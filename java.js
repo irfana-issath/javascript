ebc98ed7d7988c946ecd9323dc83946a715d840b
irfanaissath <irfanaissath755@gmail.com>
2025-06-27 13:50:52 +0530

    java.js and java.html created

---
+++ java.js
@@ -0,0 +1,35 @@
+//ingriment
+
+//post increment
+
+// let a= 5
+// const b=a ;
+// a++
+
+// console.log(a,b)
+
+
+
+//pre increment
+
+// let c= 5
+// const d =c
+
+// ++c
+
+// console.log(c,d)
+
+
+
+
+
+
+//prime numbers
+function prime(num){
+for(let i=2;i<num/2;i++){
+    if(num%i==0){
+        
+    }
+}
+}
+prime(3)
