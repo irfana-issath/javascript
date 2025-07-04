c3f4df28d7163e42db4c2ffcee100a7f9fb04e55
irfanaissath <irfanaissath755@gmail.com>
2025-07-04 17:34:00 +0530

    wwdc.html and wwdc.css edited

---
+++ dom.js
@@ -0,0 +1,11 @@
+// let element= document.getElementById("demo")
+
+// console.log(element.innerHTML)
+
+// element.innerHTML="<b>Shingu....<b>"
+
+let something = document.getElementsByTagName("h1")
+console.log(something);
+// something[1].innerHTML="<h5>izza</h5>"
+
+
