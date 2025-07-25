cbce9441615a60106b1f5f3e52ed962f23f65992
irfanaissath <irfanaissath755@gmail.com>
2025-07-25 13:40:19 +0530

    reactjs created

---
+++ reactjs/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+ <div>
+  <h1>izzaaaa</h1>
+  <h1>zaad</h1>
+ </div>
+ 
+)
