99001aaa95f70b59a9ce0abaf737dc8387a78081
irfanaissath <irfanaissath755@gmail.com>
2025-08-01 16:51:58 +0530

    mapping created

---
+++ mapping/src/main.jsx
@@ -0,0 +1,9 @@
+const container = document.getElementById("root")
+
+console.log(container.innerHTML);
+container.innerHTML="<h2>welcome</h2>"
+
+const style=container.style
+
+style.color= "white"
+style.backgroundColor= "black"
