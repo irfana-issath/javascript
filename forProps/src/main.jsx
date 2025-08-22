8bbaa8002c24855b5cccb5e6708470af9a39ce79
irfanaissath <irfanaissath755@gmail.com>
2025-08-22 16:48:28 +0530

    forProps created

--- forProps/src/main.jsx
+++ forProps/src/main.jsx
@@ -1,15 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
- <div>
-  <h1>izzaaaa</h1>
-  <h1>zaad</h1>
- </div>
- 
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
-
-
-
