ae7cd2443d1a1384c97b1c15fa3dbf43a0fe9342
irfanaissath <irfanaissath755@gmail.com>
2025-09-02 12:12:33 +0530

    function.html and function.js created

--- formUseState/src/App.jsx
+++ formUseState/src/App.jsx
@@ -33,3 +33,153 @@ function App() {
 }
 
 export default App
+
+
+
+import React, { useState } from 'react'
+
+function Form() {
+  const [name,setName] = useState();
+
+  const handleChange = (event) =>{
+    setName(event.target.value)
+    console.log(event);
+    
+  }
+
+  const handleSubmit = (e) => {
+    alert(`You typed ${name}`)
+    setName("")
+
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        Name:
+      <input type="text"
+        placeholder='Enter the name'
+        onChange={handleChange}
+        value={name} />
+
+
+
+        <button type='submit'>Submit</button>
+      </form>
+      
+    </div>
+  )
+}
+
+// export default Form
+
+// import React, { useState } from 'react'
+
+// function Form() {
+//   const [name,setName] = useState();
+
+//   const handleChange = (event) =>{
+//     setName(event.target.value)
+//     console.log(event);
+    
+//   }
+
+//   const handleSubmit = (e) => {
+//     alert(`You typed ${name}`)
+//     setName("")
+
+//   }
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         Name:
+//       <input type="text"
+//         placeholder='Enter the name'
+//         onChange={handleChange}
+//         value={name} />
+
+
+
+//         <button type='submit'>Submit</button>
+//       </form>
+      
+//     </div>
+
+
+// -------------------------------------------------------------------------------------
+
+// import React, { useState } from 'react'
+
+// function FormMult() {
+
+//   const [formData,setFormData] = useState ({
+//     name:"",
+//     email:"",
+//     number:""
+//   })
+
+//   const handleChange = (event) =>{
+//     const {name,value} = event.target // get field value & name
+//     setFormData({
+//       ...formData, 
+//       [name]:[value]   // update the field
+//     })
+//   }
+
+//   const handleSubmit =(e) =>{
+//     e.preventDefault();  //prevent page refresh
+//     alert(`you typed: 
+//        Name: ${formData.name}
+//        Email: ${formData.email}
+//        Number:${formData.number}
+//     `)
+//     setFormData({
+//       name:"",
+//       email:"",
+//       number:""
+//     })
+//   }
+
+//   return (
+//     <div>
+
+//       <form onSubmit={handleSubmit}>
+//         <label >Name</label>
+//         <input 
+//             type="text" 
+//             name="name"
+//             value={formData.name}
+//             onChange={handleChange}
+//             placeholder='Enter name'
+//         /> <br /> <br />
+
+//         <label >Email</label>
+//         <input 
+//             type="email" 
+//             name="email"
+//             value={formData.email}
+//             onChange={handleChange}
+//             placeholder='Enter email'
+//         /> <br /> <br />
+
+
+//         <label >Number</label>
+//         <input 
+//             type="number" 
+//             name="number"
+//             value={formData.number}
+//             onChange={handleChange}
+//             placeholder='Enter number'
+//         /> <br /> <br />
+
+//         <button type='submit'> Submit</button>
+
+
+//       </form>
+
+//     </div>
+//   )
+// }
+
+// export default FormMult
---
+++ function.js
@@ -0,0 +1,77 @@
+// function with argument with return value
+
+function sum(num1,num2) {
+    return num1+num2;
+}
+
+console.log(sum(50,20))
+
+
+// function without argument with return value
+function multiple() {
+    let a=5
+    let b=2
+    const res=a*b
+    return res
+
+}
+
+console.log(multiple())
+
+
+// function with argument without return value
+function sub(num1,num2) {
+    let res=num1-num2
+    console.log(res)
+}
+
+sub(100,20)
+
+
+// function without argument without return value
+function fun(){
+    console.log("hello")
+}
+
+fun()
+
+// task
+//  --------------------1-------------------------------------
+function greet(name) {
+    return `Hello ${name}`
+}
+console.log(greet("izza"))
+
+
+// ---------------------2-------------------------------------------
+function sum(a,b){
+    return a+b
+}
+console.log(sum(10,5));
+
+// ---------------------3-----------------------------------------
+function max(a,b){
+    let res= a>b? a : b;
+    return res
+}
+console.log(max(10,50));
+
+// ---------------------4----------------------------------
+
+function isEven(num){
+     return num%2==0
+}
+
+console.log(isEven(6));
+
+
+// --------------------5-----------------------------------------
+
+function findLargest(arr){
+    for(i=0;i<arr.length;i++){
+       let result= arr[i]>arr[i]? arr[i] : arr[i]
+      return result
+    }
+    
+}
+console.log(findLargest([20,30,40,50]));
